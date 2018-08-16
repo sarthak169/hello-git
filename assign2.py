a339f4dd53785106a41d07f1b97e32549e4b707e
sarthak jain <38503060+sarthak169@users.noreply.github.com>
2018-08-17 00:14:52 +0530

    assignment 2

---
+++ assign2.py
@@ -0,0 +1,24 @@
+#Question 1
+print("Hello my name is Sarthak")
+#ques 2
+st1='String1'
+st2='string2'
+st3=st1+st2
+print(st3)
+#Question 3
+x=input("enter x")
+y=input("Eneter y")
+z=input("Eneter z")
+print(x,y,z)
+#question 4
+print("Lets get started")
+#Question 5
+s='Acadview'
+course='Python'
+fees=5000
+print("Name: %s , Course: %s , Fees: %d" %(s,course,fees))
+
+#Question 6
+name="Tony Stark"
+salary=1000000
+print('%s %d'%(name,salary) )
