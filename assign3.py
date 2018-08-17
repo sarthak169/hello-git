21c1e714e0d25eb8ec6abaedadade7bb5e5aa5a9
sarthak jain <38503060+sarthak169@users.noreply.github.com>
2018-08-17 17:34:53 +0530

    Update assign3.py

--- assign3.py
+++ assign3.py
@@ -1,6 +1,6 @@
 #assignment 3
 #ques 1
-list=[]
+"""list=[]
 n=int(input("How many items do you want ot enter in the list\n"))
 for i in range(0,n):
     c=input("Enter Element\n")
@@ -29,7 +29,7 @@ b= [7,11,35,46,68,456]
 c=a+ b
 c.sort()
 print(c)
-#ques 6
+#optional ques
 stack=['a','b','c','d','e']
 print("Stack is: ",stack)
 stack.append('f')
@@ -43,7 +43,7 @@ queue.append(99)
 print("New queue is: ",queue)
 queue.pop(0)
 print("Updated queue is: ",queue)
-#optional ques
+#ques 6
 even=0
 odd=0
 for i in c:
@@ -52,3 +52,19 @@ for i in c:
     else:
         odd+=1
 print("Even Elements are\n",even,"\nodd elements are \n",odd)
+#ques 7"""
+tup=('hello','i','am','sarthak')
+print(tup[::-1])
+tup1=(4,6,2,1,2,3,10,24,1424,123)
+print("Max element is",max(tup1))
+print("Smallest element is ",min(tup1))
+#Strings
+#ques 1
+st="welcome to python"
+print(st.upper())
+#ques2
+st1="1234"
+print(st1.isdigit())
+#ques 3
+str="Hello World"
+print(str.replace('World','Sarthak'))
