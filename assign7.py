27c891a212058ac22c93bd776a62896455de7b6f
sarthak jain <38503060+sarthak169@users.noreply.github.com>
2018-08-23 23:29:15 +0530

    Add files via upload

---
+++ assign7.py
@@ -0,0 +1,16 @@
+#ques 1
+a=eval(input('enter dictionary'))
+b=int(input('enter the value'))
+for k,v in a.items():
+    if b==v:
+        break
+print(k)
+
+#ques 2
+a={'sarthak':{'maths':30,'physics':30,'chem':90},'rajat':{'maths':90,'physics':50,'chem':10},'vipul':{'maths':78,'physics':30,'chem':60}}
+b=str(input('enter name of student'))
+for k,v in a.items():
+    if b==k:
+        print('maths=',a[k]['maths'])
+        print('physics=',a[k]['physics'])
+        print('chem=',a[k]['chem'])
