89497905c4330e8e6569845e6034cc61cc52dbea
sarthak jain <38503060+sarthak169@users.noreply.github.com>
2018-08-31 19:06:41 +0530

    Add files via upload

--- assign11.py
+++ assign11.py
@@ -1,4 +1,7 @@
-#ques 1
-[a-zA-Z_0-9.]*[@](gmail.com|yahoo.com)
-#ques 2
-^+91[6-9]{1}[0-9]{9}
+from datetime import date
+days=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+print(days[date.weekday(date.today())])
+import webbrowser
+webbrowser.open('https://www.youtube.com/watch?v=QJbpJQscn9E&t=113s')
+import os
+os.rename('file.txt','file.jpg')
